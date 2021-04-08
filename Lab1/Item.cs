0a4dde113ac871fb1a29dd9c94e2f6f6d29c8f6b
Ilya Sereda <ilyasereda121@gmail.com>
2021-04-08 11:43:27 +0300

    Added comments to class Item<T>

--- Lab1/Item.cs
+++ Lab1/Item.cs
@@ -6,18 +6,34 @@ namespace Lab1
 {
     class Item<T>
     {
+        /// <summary>
+        /// Данные хранимые в ячейке списка
+        /// </summary>
         public T Data;
-
+        /// <summary>
+        /// Следующая ячейка списка
+        /// </summary>
         public Item<T> Next;
-
+        /// <summary>
+        /// Конструктор элемента
+        /// </summary>
+        /// <param name="data"></param>
         public Item(T data)
         {
             Data = data;
         }
+        /// <summary>
+        /// Задать значение ячейке
+        /// </summary>
+        /// <param name="i"></param>
         public void setItem(T i)
         {
             Data = i;
         }
+        /// <summary>
+        /// Задать значение следующему элементу
+        /// </summary>
+        /// <param name="n"></param>
         public void setNext(Item<T> n)
         {
             Next = n;
