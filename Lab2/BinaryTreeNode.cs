8f1abaa14f12ba5941ca58579175997c182f54c0
Ilya Sereda <ilyasereda121@gmail.com>
2021-04-22 12:28:57 +0300

    Added Elements of class

--- Lab2/BinaryTreeNode.cs
+++ Lab2/BinaryTreeNode.cs
@@ -4,7 +4,39 @@ using System.Text;
 
 namespace Lab2
 {
-    class BinaryTreeNode
+    class BinaryTreeNode<T> where T : IComparable
     {
+        /// <summary>
+        /// Расположения узла относительно родителя
+        /// </summary>
+        public enum Side
+        {
+            Left,
+            Right
+        }
+        /// <summary>
+        /// Данные которые хранятся в узле
+        /// </summary>
+        public int Data;
+
+        /// <summary>
+        /// Левая ветка
+        /// </summary>
+        public BinaryTreeNode<T> LeftNode;
+
+        /// <summary>
+        /// Правая ветка
+        /// </summary>
+        public BinaryTreeNode<T> RightNode;
+
+        /// <summary>
+        /// Родитель
+        /// </summary>
+        public BinaryTreeNode<T> ParentNode;
+
+        /// <summary>
+        /// Расположение узла относительно его родителя
+        /// </summary>
+        public Side? NodeSide => ParentNode == null ? (Side?)null : ParentNode.LeftNode == this ? Side.Left : Side.Right;
     }
 }
