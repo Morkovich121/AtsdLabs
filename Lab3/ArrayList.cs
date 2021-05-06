095bdff2f21885835d2cef205aa200554575c700
Ilya Sereda <ilyasereda121@gmail.com>
2021-05-06 12:21:06 +0300

    Added ArrList Constuctor

--- Lab3/ArrayList.cs
+++ Lab3/ArrayList.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace lab3
 {
-    class Class1
+    class ArrayList<T> where T : IComparable
     {
+        public object[] array;
+        public int max;
+        int last;
+        public ArrayList(int m = 10)
+        {
+            max = m;
+            array = new object[max];
+            last = -1;
+        }
+
+        public bool isFull()
+        {
+            return last >= array.Length - 1;
+        }
+        public void AddItem(object item)
+        {
+            if (isFull()) resize(ref array, array.Length + 1);
+            array[++last] = item;
+
+        }
+        private void resize(ref object[] array, int leng)
+        {
+            object[] array2 = new object[leng];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            array = array2;
+        }
     }
 }
