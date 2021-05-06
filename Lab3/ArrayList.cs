3a615fef9b160604f2f14b5c9eb3509b47a1000e
Ilya Sereda <ilyasereda121@gmail.com>
2021-05-06 13:13:47 +0300

    Added comments to all methods

--- Lab3/ArrayList.cs
+++ Lab3/ArrayList.cs
@@ -9,31 +9,55 @@ namespace lab3
         public T[] array;
         public int max;
         int last;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="m"></param>
         public ArrayList(int m = 10)
         {
             max = m;
             array = new T[max];
             last = -1;
         }
-
+        /// <summary>
+        /// Полный ли массив
+        /// </summary>
+        /// <returns></returns>
         public bool isFull()
         {
             return last >= array.Length - 1;
         }
+        /// <summary>
+        /// Пустой ли массив
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
             return last == -1;
         }
+        /// <summary>
+        /// Длина массива
+        /// </summary>
+        /// <returns></returns>
         public int Size()
         {
             return array.Length;
         }
+        /// <summary>
+        /// Добавление элемента в массив
+        /// </summary>
+        /// <param name="item"></param>
         public void AddItem(T item)
         {
             if (isFull()) Resize(ref array, array.Length + 1);
             array[++last] = item;
 
         }
+        /// <summary>
+        /// Изменение максимального размера массива и его перезапись
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="leng"></param>
         private void Resize(ref T[] array, int leng)
         {
             T[] array2 = new T[leng];
@@ -43,7 +67,9 @@ namespace lab3
             }
             array = array2;
         }
-
+        /// <summary>
+        /// Вывод списка
+        /// </summary>
         public void PrintList()
         {
             for(int i = 0; i <= last; i++)
@@ -52,7 +78,7 @@ namespace lab3
             }
             Console.WriteLine();
         }
-        public void Heapify(T[] array, int pos, int size)
+        private void Heapify(T[] array, int pos, int size)
         {
             T temp;
             while (2 * pos + 1 < size)
@@ -72,13 +98,17 @@ namespace lab3
                 else break;
             }
         }
-        public void HeapMake(T[] array, int n)
+        private void HeapMake(T[] array, int n)
         {
             for (int i = n - 1; i >= 0; i--)
             {
                 Heapify(array, i, n);
             }
         }
+
+        /// <summary>
+        /// Сортировка пирамидой
+        /// </summary>
         public void HeapSort()
         {
             T temp;
