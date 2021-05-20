51c64174ff1aead545f3698e837253bb46e62978
Ilya Sereda <ilyasereda121@gmail.com>
2021-05-20 18:46:50 +0300

    Added constructor and elements to LinkedList class

--- Labs4/LinkedList.cs
+++ Labs4/LinkedList.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Labs4
 {
-    class LinkedList
+    public class LinkedList<T> where T : IComparable<T>
     {
+        public Item<T> Head;
+        public Item<T> Tail;
+        public int Count;
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+        public LinkedList(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
     }
-}
