023aa0124dd9f8c676fc57a9072c0693a0ea0bf7
Ilya Sereda <ilyasereda121@gmail.com>
2021-05-20 18:55:07 +0300

    Added some tests

--- Labs4/Program.cs
+++ Labs4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Labs4
 {
@@ -6,7 +7,19 @@ namespace Labs4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Укажите кол-во вершин графа");
+            var a = Console.ReadLine();
+            Console.WriteLine("Укажите кол-во ребер графа");
+            var b = Console.ReadLine();
+            var graph = new GraphWeighted(Convert.ToInt32(a), Convert.ToInt32(b));
+            graph.PrintGraph();
+            Console.WriteLine();
+            graph.Kruskal();
+            graph.Dijkstra(1);
+            var MST = new List<edge_t>();
+            var arr = new LinkedList<string>();
+            graph.Prim(arr);
+            arr.PrintList();
         }
     }
 }
